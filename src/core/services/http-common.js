4148c5330aedc9e559f6f88fa03ed62ada1e14fc
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-11-19 15:48:56 -0500

    feat: ApiLinkUrl service added

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {storeGlobal} from "@/learning/store/store";
 
 export default axios.create({
-   baseURL: 'https://localhost:44396/api/v1',
+   baseURL: 'https://learningcenterapi20231119153224.azurewebsites.net/api/v1',
    headers: {
       'Content-type': 'application/json',
       ...(storeGlobal.user && storeGlobal.user.token
